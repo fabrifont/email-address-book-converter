7af7d1157dda6829e9c7af0594525f395e1c82d5
Fabrizio Fontanarrosa <fabriziofontanarrosa@gmail.com>
2025-04-28 16:33:54 -0300

    Formateado en funciones, hay que comenzar la GUI

--- csv_utils.py
+++ csv_utils.py
@@ -1,64 +1,61 @@
 import pandas as pd
 import chardet as cd
 
+column_mapping = {
+     'First Name': ['Primer nombre', 'Nombre'],
+     'Last Name': ['Apellido', 'Apellidos'],
+     'Nickname': ['Apodo'],
+     #'Display Name': ['NO Nombre mostrado', 'Nombre en pantalla'],
+     'E-mail Address': ['Dirección de correo electrónico principal'],
+     'E-mail 2 Address': ['Dirección de correo electrónico secundaria'],
+     'Business Phone': ['Teléfono (Trabajo)'],
+     'Home Phone': ['Teléfono particular'],
+     'Business Fax': ['Fax'],
+     'Pager': ['Buscapersonas'],
+     'Mobile Phone': ['Teléfono móvil'],
+     'Home Street': ['Dirección personal'],
+     'Home City': ['Ciudad donde vive'],
+     'Home State': ['Provincia'],
+     'Home Postal Code': ['Código postal'],
+     'Home Country/Region': ['País de residencia'],
+     'Business Street': ['Dirección de trabajo'],
+     'Business City': ['Ciudad (Trabajo)'],
+     'Business State': ['Provincia (Trabajo)'],
+     'Business Postal Code': ['Código postal (Trabajo)'],
+     'Business Country/Region': ['País (Trabajo)'],
+     'Job Title': ['Cargo', 'Puesto'],
+     'Department': ['Departamento'],
+     'Company': ['Compañía', 'Organización'],
+     'Web Page': ['Página web 1'],
+     'Notes': ['Notas']
+}
+
 def detect_encoding(filename, bytes=10000):
      with open(filename, 'rb') as f:
           encoding = cd.detect(f.read(bytes))
      print(encoding['confidence'])
      return encoding['encoding']
 
+def convert_csv_utf8(file):
+     new_filename = file.replace(".csv", "") + "_utf8.csv"
+     old_encoding = detect_encoding(file)
+     df_no_utf8 = pd.read_csv(file, encoding=old_encoding)
+     df_no_utf8.to_csv(new_filename, index=False, encoding='utf-8-sig')
+     return new_filename
 
-filename = "test_contactos.csv"
-new_filename = filename.replace(".csv", "") + "_converted.csv"
-old_encoding = detect_encoding(filename)
+def format_csv(file):
+     df = pd.read_csv(file)
+     new_filename = file.replace(".csv", "") + "_CONVERTED.csv"
+     for column in df.columns:
+          found = 0
+          for new_title in column_mapping:
+               if(column in column_mapping[new_title]):
+                    df.rename(columns={column:new_title}, inplace=True)
+                    found = 1
+                    break
+          if(not found):
+               df = df.drop(column, axis=1)
+     return df
 
-df_no_utf8 = pd.read_csv(filename, encoding=old_encoding)
-df_no_utf8.to_csv(new_filename, index=False, encoding='utf-8-sig')
-
-df = pd.read_csv(new_filename)
-
-column_mapping = {
-    'First Name': ['Primer nombre', 'Nombre'],
-    'Last Name': ['Apellido', 'Apellidos'],
-    'Nickname': ['Apodo'],
-    #'Display Name': ['NO Nombre mostrado', 'Nombre en pantalla'],
-    'E-mail Address': ['Dirección de correo electrónico principal'],
-    'E-mail 2 Address': ['Dirección de correo electrónico secundaria'],
-    'Business Phone': ['Teléfono (Trabajo)'],
-    'Home Phone': ['Teléfono particular'],
-    'Business Fax': ['Fax'],
-    'Pager': ['Buscapersonas'],
-    'Mobile Phone': ['Teléfono móvil'],
-    'Home Street': ['Dirección personal'],
-    'Home City': ['Ciudad donde vive'],
-    'Home State': ['Provincia'],
-    'Home Postal Code': ['Código postal'],
-    'Home Country/Region': ['País de residencia'],
-    'Business Street': ['Dirección de trabajo'],
-    'Business City': ['Ciudad (Trabajo)'],
-    'Business State': ['Provincia (Trabajo)'],
-    'Business Postal Code': ['Código postal (Trabajo)'],
-    'Business Country/Region': ['País (Trabajo)'],
-    'Job Title': ['Cargo', 'Puesto'],
-    'Department': ['Departamento'],
-    'Company': ['Compañía', 'Organización'],
-    'Web Page': ['Página web 1'],
-    'Notes': ['Notas']
-}
-
-num_of_columns = df.shape[1]
-
-for column in df.columns:
-     found = 0
-     for new_title in column_mapping:
-          if(column in column_mapping[new_title]):
-               df.rename(columns={column:new_title}, inplace=True)
-               found = 1
-               break
-     if(not found):
-          df = df.drop(column, axis=1)
-
-
-
-
-df.to_csv(new_filename, index=False)
+def save_csv(df, new_filename):
+     df.to_csv(new_filename, index=False)
